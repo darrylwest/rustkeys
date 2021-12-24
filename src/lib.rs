3a4d555af2f0559b9d5cac5be029f08713e96439
darryl west <dpw@raincitysoftware.com>
2021-12-24 10:19:10 -0800

    implemented simple bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,46 @@
+use rand::prelude::*;
 
+pub fn bytes(size: usize) -> Vec<u8> {
+    const COUNT: usize = 64;
+    let mut data = [0u8; COUNT];
+    rand::thread_rng().fill_bytes(&mut data);
+
+    let sz: usize = if size > COUNT {
+        COUNT
+    } else {
+        size
+    };
+
+    let mut v = vec![];
+    for i in 0..sz {
+        v.push(data[i]);
+    }
+
+    v
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::bytes;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn bytes_min_test() {
+        let v = bytes(2);
+        assert_eq!(v.len(), 2);
+        println!("{:?}", v);
+    }
+
+    #[test]
+    fn bytes_max_test() {
+        let v = bytes(64);
+        assert_eq!(v.len(), 64);
+        println!("{:?}", v);
+    }
+
+    #[test]
+    fn bytes_over_max_test() {
+        let v = bytes(128);
+        assert_eq!(v.len(), 64);
+        println!("{:?}", v);
     }
 }
